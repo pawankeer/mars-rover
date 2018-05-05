9b264febc6013bb5c44351586bd55cc9e5abb017
pawankeer <pawandeep.keer@gmail.com>
2018-05-04 20:52:19 -0400

    refactored tests

--- src/action-turn.test.js
+++ src/action-turn.test.js
@@ -1,58 +1,58 @@
 const assert = require('assert');
 const {actionTurn} = require('./action-turn');
 
-describe('should test action', function () {
-    describe('Current orientation N', function () {
+describe('action turn', function () {
+    describe('current orientation N', function () {
         const currentOrientation = 'N';
-        it('turns W for direction L', function () {
+        it('should turn W for direction L', function () {
             const direction = 'L';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'W');
         });
 
-        it('turns E for direction R', function () {
+        it('should turn E for direction R', function () {
             const direction = 'R';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'E');
         });
     });
-    describe('Current orientation S', function () {
+    describe('current orientation S', function () {
         const currentOrientation = 'S';
-        it('turns W for direction L', function () {
+        it('should turn W for direction L', function () {
             const direction = 'L';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'E');
         });
 
-        it('turns E for direction R', function () {
+        it('should turn E for direction R', function () {
             const direction = 'R';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'W');
         });
     });
-    describe('Current orientation W', function () {
+    describe('current orientation W', function () {
         const currentOrientation = 'W';
-        it('turns W for direction L', function () {
+        it('should turn W for direction L', function () {
             const direction = 'L';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'S');
         });
 
-        it('turns E for direction R', function () {
+        it('should turn E for direction R', function () {
             const direction = 'R';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'N');
         });
     });
-    describe('Current orientation E', function () {
+    describe('current orientation E', function () {
         const currentOrientation = 'E';
-        it('turns W for direction L', function () {
+        it('should turn W for direction L', function () {
             const direction = 'L';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'N');
         });
 
-        it('turns E for direction R', function () {
+        it('should turn E for direction R', function () {
             const direction = 'R';
             const newOrientation = actionTurn(currentOrientation, direction);
             assert.equal(newOrientation, 'S');
--- src/step.test.js
+++ src/step.test.js
@@ -1,33 +1,35 @@
 const assert = require('assert');
 const { step } = require('./step');
 
-describe('should test step', function () {
-   it('makes coordinates (0,1) when N and (0,0)', function () {
-       const currentOrrientation = 'N';
-       const currentCoordinates = { x: 0, y: 0 };
-       const newCoordinates = step(currentOrrientation, currentCoordinates);
-       assert.equal(newCoordinates.x, 0);
-       assert.equal(newCoordinates.y, 1);
-   });
-    it('makes coordinates (0,-1) when S and (0,0)', function () {
-        const currentOrrientation = 'S';
-        const currentCoordinates = { x: 0, y: 0 };
-        const newCoordinates = step(currentOrrientation, currentCoordinates);
-        assert.equal(newCoordinates.x, 0);
-        assert.equal(newCoordinates.y, -1);
-    });
-    it('makes coordinates (-1,0) when W and (0,0)', function () {
-        const currentOrrientation = 'W';
-        const currentCoordinates = { x: 0, y: 0 };
-        const newCoordinates = step(currentOrrientation, currentCoordinates);
-        assert.equal(newCoordinates.x, -1);
-        assert.equal(newCoordinates.y, 0);
-    });
-    it('makes coordinates (1,0) when E and (0,0)', function () {
-        const currentOrrientation = 'E';
-        const currentCoordinates = { x: 0, y: 0 };
-        const newCoordinates = step(currentOrrientation, currentCoordinates);
-        assert.equal(newCoordinates.x, 1);
-        assert.equal(newCoordinates.y, 0);
+describe('step', function () {
+    describe('current coordinates (0,0)', function () {
+        it('should make coordinates (0,1) for orientation N', function () {
+            const currentOrientation = 'N';
+            const currentCoordinates = { x: 0, y: 0 };
+            const newCoordinates = step(currentOrientation, currentCoordinates);
+            assert.equal(newCoordinates.x, 0);
+            assert.equal(newCoordinates.y, 1);
+        });
+        it('should make coordinates (0,-1) for orientation S', function () {
+            const currentOrientation = 'S';
+            const currentCoordinates = { x: 0, y: 0 };
+            const newCoordinates = step(currentOrientation, currentCoordinates);
+            assert.equal(newCoordinates.x, 0);
+            assert.equal(newCoordinates.y, -1);
+        });
+        it('should make coordinates (-1,0) for orientation W', function () {
+            const currentOrientation = 'W';
+            const currentCoordinates = { x: 0, y: 0 };
+            const newCoordinates = step(currentOrientation, currentCoordinates);
+            assert.equal(newCoordinates.x, -1);
+            assert.equal(newCoordinates.y, 0);
+        });
+        it('should make coordinates (1,0) for orientation E', function () {
+            const currentOrientation = 'E';
+            const currentCoordinates = { x: 0, y: 0 };
+            const newCoordinates = step(currentOrientation, currentCoordinates);
+            assert.equal(newCoordinates.x, 1);
+            assert.equal(newCoordinates.y, 0);
+        });
     });
 });
